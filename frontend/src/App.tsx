5b7c4dbda2bff20b1f635eb079944094b7b89f98
Victor Wanderley Barbosa <vwb@shl.dk>
2025-11-10 10:43:37 +0100

    feat: hellow world app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,58 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import revitLogo from "./assets/revit.svg";
-import rhinoLogo from "./assets/rhino.png";
+import HelloWorldApp from "./components/hello-world";
 import { ThemeProvider } from "./components/theme-provider";
-import ThemeToggle from "./components/theme-toggle";
-import { Button } from "./components/ui/button";
-import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const host: string = "Rhino";
-  let logo: string = reactLogo;
-  switch (host) {
-    case "Rhino":
-      logo = rhinoLogo;
-      break;
-    case "Revit":
-      logo = revitLogo;
-      break;
-    case "React":
-    default:
-      logo = reactLogo;
-      break;
-  }
-
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <div className="flex flex-col min-h-screen items-center justify-center">
-        <div className="flex gap-18 mb-8">
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="size-42" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img
-              src={logo}
-              className="size-42 animate-[spin_10s_linear_infinite]"
-              alt="Host logo"
-            />
-          </a>
-        </div>
-        <h1 className="text-6xl font-bold">Vite + {host}</h1>
-        <div className="flex flex-col items-center gap-4 mt-8">
-          <div className="flex gap-4">
-            <Button onClick={() => setCount((count) => count + 1)}>
-              Count is {count}
-            </Button>
-            <ThemeToggle />
-          </div>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-      </div>
+      <HelloWorldApp />
     </ThemeProvider>
   );
 }
